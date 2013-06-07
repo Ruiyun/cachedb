2e1e5d89b0554142f56d9b8f17b61c535aea5f32
Ruiyun <ruiyun.wen@gmail.com>
2013-06-07 22:25:23 +0800

    work in progress
    upsert almost ready, but not test yet.

--- src/cachedb/core.clj
+++ src/cachedb/core.clj
@@ -25,52 +25,82 @@
   (-> (assoc-in coll [:data id] doc)
       (update-index conj doc [(:id doc)])))
 
-(defn insert [coll doc]
-  {:pre [(validate-keys? coll (keys doc))]}
+(defn- insert-internal [coll doc]
   (if-let [id (:id doc)]
     (when-not (contains? (:data coll) id)
       (insert-doc coll doc))
     (->> (assoc doc :id (ObjectId.))
          (insert-doc coll))))
 
-(defn- use-index? [{:keys [idx] :as coll} condition]
+(defn insert [coll doc]
+  {:pre [(validate-keys? coll (keys doc))]}
+  (insert-internal coll doc))
+
+(defn- use-index? [{:keys [idx]} condition]
   (every? (-> (keys idx) (set)) (keys condition)))
+
+(defn- get-effect-ids-in-index [{:keys [idx]} condition]
+  (->> (keys condition)
+       (map #(get-in idx [% (get condition %)]))
+       (apply clojure.set/intersection)))
+
+(defn- match-condition [condition doc]
+  (every? (fn [[k v]] (= (k doc) v)) condition))
+
+(defn- get-effect-ids [{:keys [data] :as coll} {cond-id :id :as condition}]
+  (cond
+   cond-id (when (get data cond-id) #{cond-id})
+   (use-index? coll condition) (get-effect-ids coll condition)
+   :else (->> (filter (partial match-condition condition) (vals data))
+              (reduce #(conj (or %1 #{}) (:id %2)) nil))))
+
+(defn- get-docs-by-ids [data ids]
+  (reduce #(->> (get data %2) (conj %1)) [] ids))
+
+(defn- update-index-by-multi-condition [coll ids conditions doc]
+  (let [doc-keys (keys doc)]
+    (-> (reduce #(update-index %1 disj (select-keys %2 doc-keys) ids) coll conditions)
+        (update-index conj doc ids))))
+
+(defn- update-docs-by-ids [{:keys [data] :as coll} ids doc]
+  (let [old-docs (get-docs-by-ids data ids)]
+      (-> (reduce #(update-in %1 [:data %2] merge doc) coll ids)
+          (update-index-by-multi-condition ids old-docs doc))))
+
+(defn- update-internal [coll condition doc]
+  (if-let [ids (get-effect-ids coll condition)]
+    (update-docs-by-ids coll ids doc)
+    coll))
 
 (defn update [coll condition doc]
   {:pre [(validate-keys? coll (keys doc))
          (not (contains? doc :id))]}
-  (if-let [ids (cond
-                (contains? condition :id) [(:id condition)]
-                (use-index? coll condition) (->> (keys condition)
-                                                 (map #(get-in coll [:idx % (get condition %)]))
-                                                 (apply clojure.set/intersection))
-                :else (->> (filter #(every? (fn [[k v]] (= (k %) v)) condition) (-> (:data coll) (vals)))
-                           (map :id)))]
-    (let [doc-keys (keys doc)
-          old-docs (reduce (fn [docs id]
-                             (->> (-> (get (:data coll) id)
-                                      (select-keys doc-keys))
-                                  (conj docs)))
-                           [] ids)]
-      (-> (reduce #(update-in %1 [:data %2] merge doc) coll ids)
-          ((fn [coll]
-             (reduce #(update-index %1 disj %2 ids) coll old-docs)))
-          (update-index conj doc ids)))
-    coll))
+  (update-internal coll condition doc))
+
+(defn- exists-internal [{:keys [data] :as coll} {cond-id :id :as condition}]
+  (cond
+   cond-id (contains? data cond-id)
+   (use-index? coll condition) (not (nil? (-> (get-effect-ids-in-index coll condition) (seq))))
+   :else (not (nil? (some (partial match-condition condition) (vals data))))))
+
+(defn exists? [coll condition]
+  (exists-internal coll condition))
+
+(defn upsert [coll condition doc]
+  {:pre [(validate-keys? coll (keys doc))
+         (not-every? #(contains? % :id) [condition doc])]}
+  (if-let [ids (get-effect-ids coll condition)]
+    (update-docs-by-ids coll ids doc)
+    (->> (merge condition doc) (insert-internal coll))))
 
 (def user (collection [:a :b :c] [:a :b]))
 (-> user
     (insert {:id 1 :a 1 :b 2 :c 3})
     (insert {:id 2 :a 1 :b 1 :c 4})
     (insert {:id 3 :a 2 :b 1 :c 3})
-    (update {:c 3} {:a 3 :b 3 :c 5})
+    (update {:id 4} {:a 3 :b 3 :c 5})
+    (exists? {:a 2 :c 4})
     )
-
-(defn upsert [coll condition doc]
-  {:pre [(validate-keys? coll (keys doc))
-         (not-every? #(contains? % :id) [condition doc])]}
-  ;; TODO
-  )
 
 (comment
   ;; TODO 根据新结构重新实现
